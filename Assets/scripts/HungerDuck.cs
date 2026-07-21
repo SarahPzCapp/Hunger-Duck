b7f1a39a7c9ea36b77037cd06fc5f86966d790e2
SarahPzCapp <sarah_mazzanti@estudante.sesisenai.org.br>
2026-07-21 17:11:52 -0300

    cu

--- Assets/scripts/HungerDuck.cs
+++ Assets/scripts/HungerDuck.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HungerDuck : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public InputAction moveKeys;
+    Rigidbody2D rb;
+    int velocity = 5;
+    public GameObject sprite;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        
+        Movement();
+    }
+
+    void Movement()
+    {
+        var direction = moveKeys.ReadValue<float>();
+        if (direction != 0)
+        {
+            rb.linearVelocity = new Vector2 (direction * velocity,rb.linearVelocityY);
+        }     
+    }
+
+    void OnEnable()
+    {
+        moveKeys.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveKeys.Disable();
     }
 }
